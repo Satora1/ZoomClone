47c9860188f20563b3040a8fa9efc1358cf77516
Jakub Satora <kubasatora@wp.pl>
2024-04-05 12:48:13 +0200

    sidebar and icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/lib/utils';
 import { Activity } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
+import Image from 'next/image'
 import React from 'react'
 
 const Sidebar = () => {
@@ -15,16 +16,23 @@ const Sidebar = () => {
             <div className='flex flex- flex-col gap-6'>
 
                 {sidebarLinks.map((link) => {
-                    const isActive = pathname === link.route || pathname.startsWith(link.route);
-                    return(
+                    const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
+                    return (
                         <Link
-                        href={link.route}
-                        key={link.label}
-                        className={cn("flex gap-4 items-center p-4 rounded-lg justify-start",{
-                            "bg-blue-1":isActive,
-                        })}>
-{link.label}
-
+                            href={link.route}
+                            key={link.label}
+                            className={cn("flex gap-4 items-center p-4 rounded-lg justify-start", {
+                                "bg-blue-1": isActive,
+                            })}>
+                            <Image
+                                src={link.imgUrl}
+                                alt={link.label}
+                                width={24}
+                                height={24}
+                            />
+                            <p className='text-lg font-semibold max-lg:hidden'>
+                                {link.label}
+                            </p>
                         </Link>
                     )
                 })}
