e6bc141f580724345b2ff0a524dec525ac5d9c96
Jakub Satora <kubasatora@wp.pl>
2024-04-09 14:20:44 +0200

    starting main page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,27 @@
+import MeetingTypeList from '@/components/MeetingTypeList';
 import React from 'react'
 
 const Home = () => {
+  const now = new Date();
+  const time =now.toLocaleTimeString().slice(0,5)
+  const date =(new Intl.DateTimeFormat).format(now)
   return (
-<section className='flex size-full flex-col gap-10 text-white'>
-<h1 className='text-3xl font-bold'>
-Home
-</h1>
-
-</section>
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
+        <div className='flex h-full flex-col justify-between mas-md:px-5 max-md:py-8 lg:p-11'>
+          <h2 className='glassmorphism max-2-[270px] rounded py-2 text-center text-base'>
+            Upcoming meeting with your mom
+          </h2>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-4xl font-extrabold lg:text-7xl'>{time}</h1>
+            <p className='text-lg font-medium text-sky-1 lg:text-2xl'>
+             {date}
+            </p>
+          </div>
+        </div>
+      </div>
+<MeetingTypeList/>
+    </section>
   )
 }
 
