8d70aad03cf37b6b5b0340a13ee87f787a309d9f
Jakub Satora <kubasatora@wp.pl>
2024-04-11 16:23:49 +0200

    create meeting room

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -6,40 +6,48 @@ import { useRouter } from 'next/navigation'
 import MeetingModal from './MeetingModal'
 
 const MeetingTypeList = () => {
-    const router =useRouter();
-    const[meetingState,setMeetingState]=
-    useState<"isScheduleMeeting"|"isJoiningMeeting"|"isInstantMeeting"|undefined>()
+    const router = useRouter();
+    const [meetingState, setMeetingState] =
+        useState<"isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined>()
+    const createMeeting = () => { }
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols2 xl:grid-cols-4'>
             <HomeCard
-            img="/icons/add-meeting.svg"
-            title="New Meeting"
-            description="Start a meeting"
-            handleClick={()=>setMeetingState("isJoiningMeeting")}
-            className="bg-orange-1"
-            
+                img="/icons/add-meeting.svg"
+                title="New Meeting"
+                description="Start a meeting"
+                handleClick={() => setMeetingState("isInstantMeeting")}
+                className="bg-orange-1"
+
             />
             <HomeCard
-                      img="/icons/schedule.svg"
-                      title="Schedule Meeting"
-                      description="Plan your Meeting"
-                      handleClick={()=>setMeetingState("isScheduleMeeting")}
-                      className="bg-blue-1"/>
+                img="/icons/schedule.svg"
+                title="Schedule Meeting"
+                description="Plan your Meeting"
+                handleClick={() => setMeetingState("isScheduleMeeting")}
+                className="bg-blue-1" />
             <HomeCard
-                      img="/icons/recordings.svg"
-                      title="View Recordings"
-                      description="Check out your recordings"
-                      handleClick={()=>setMeetingState("isJoiningMeeting")}
-                      className="bg-purple-1"/>
+                img="/icons/recordings.svg"
+                title="View Recordings"
+                description="Check out your recordings"
+                handleClick={() => setMeetingState("isJoiningMeeting")}
+                className="bg-purple-1" />
             <HomeCard
-                      img="/icons/join-meeting.svg"
-                      title="Join Meeting"
-                      description="Via inv link"
-                      handleClick={()=>setMeetingState("isJoiningMeeting")}
+                img="/icons/join-meeting.svg"
+                title="Join Meeting"
+                description="Via inv link"
+                handleClick={() => setMeetingState("isJoiningMeeting")}
 
-                      className="bg-yellow-1"/>
-                      <MeetingModal/>
-         </section>
+                className="bg-yellow-1" />
+            <MeetingModal
+                isOpen={meetingState === "isInstantMeeting"}
+                onClose={() => setMeetingState(undefined)}
+                title="Start a meeting"
+                className="text-center"
+                buttonText="Start Meeting"
+                handleClick={createMeeting}
+            />
+        </section>
     )
 }
 
